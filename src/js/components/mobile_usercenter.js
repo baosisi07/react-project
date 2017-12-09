52df4c10e3515d5caba65ac9bb9b33c5ff4cae0b
baosisi <18018021075@163.com>
2017-12-09 22:00:54 +0800

    完善个人中心页面

--- src/js/components/mobile_usercenter.js
+++ src/js/components/mobile_usercenter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Tabs } from 'antd';
+import { Row, Col, Tabs, Upload, Card, Icon, Modal } from 'antd';
 import MobileHeader from "./mobile_header";
 import MobileFooter from "./mobile_footer";
 const TabPane = Tabs.TabPane;
@@ -7,19 +7,109 @@ class MobileUsercenter extends Component {
     constructor() {
         super();
         this.state = {
-
+            userCollection: '',
+            userComment: '',
+            previewVisible: false,
+            previewImage: '',
+            fileList: [{
+                uid: -1,
+                name: 'xxx.png',
+                status: 'done',
+                url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            }],
         }
     }
+    componentDidMount() {
+        var myFetchOption = {
+            method: 'GET'
+        };
+        fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid=' + localStorage.userId, myFetchOption)
+            .then(response => response.json())
+            .then(json => {
+
+                this.setState({
+                    userCollection: json
+                });
+            })
+
+        fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid=' + localStorage.userId, myFetchOption)
+            .then(response => response.json())
+            .then(json => {
+
+                this.setState({
+                    userComment: json
+                });
+            })
+    }
+    handleCancel = () => this.setState({
+        previewVisible: false
+    })
+
+    handlePreview = (file) => {
+        this.setState({
+            previewImage: file.url || file.thumbUrl,
+            previewVisible: true,
+        });
+    }
+
+    handleChange = ({fileList}) => this.setState({
+        fileList
+    })
     render() {
+        const {previewVisible, previewImage, fileList} = this.state;
+        const uploadButton = (
+        <div>
+        <Icon type="plus" />
+        <div className="ant-upload-text">Upload</div>
+      </div>
+        );
+        const {userCollection, userComment} = this.state;
+        const collectionList = userCollection.length ?
+            userCollection.map((cl, index) => (
+                <Card style={{
+                    marginBottom: '5px'
+                }} key={index} title={cl.uniquekey} extra={<a target="_blank" href={`/details/${cl.uniquekey}`}>查看新闻详情</a>}>
+                <p>{cl.Title}</p>   
+                </Card>
+            ))
+            :
+            "暂时没有收藏内容！";
+        const commentList = userComment.length ?
+            userComment.map((ucl, index) => (
+                <Card style={{
+                    marginBottom: '5px'
+                }} key={index} title={`于 ${ucl.datetime} 评论了文章`} extra={<a target="_blank" href={`/details/${ucl.uniquekey}`}>查看新闻详情</a>}>
+                <p>{ucl.Comments}</p>   
+                </Card>
+            ))
+            :
+            "暂时没有评论过任何新闻！";
         return (
             <div id="mobile">
                 <MobileHeader></MobileHeader>
                 <Row>
                     <Col span={24}>
                         <Tabs defaultActiveKey="1" tabPosition="top">
-                          <TabPane tab="我的收藏列表" key="1">Content of tab 1</TabPane>
-                          <TabPane tab="我的评论列表" key="2">Content of tab 2</TabPane>
-                          <TabPane tab="头像设置" key="3">Content of tab 3</TabPane>
+                          <TabPane tab="我的收藏列表" key="1">{collectionList}</TabPane>
+                          <TabPane tab="我的评论列表" key="2">{commentList}</TabPane>
+                          <TabPane tab="头像设置" key="3">
+                            <div className="clearfix">
+                                <Upload
+            action="//jsonplaceholder.typicode.com/posts/"
+            listType="picture-card"
+            fileList={fileList}
+            onPreview={this.handlePreview}
+            onChange={this.handleChange}
+            >
+                                  {fileList.length >= 3 ? null : uploadButton}
+                                </Upload>
+                                <Modal visible={previewVisible} footer={null} onCancel={this.handleCancel}>
+                                  <img alt="example" style={{
+                width: '100%'
+            }} src={previewImage} />
+                                </Modal>
+                              </div>
+                          </TabPane>
                         </Tabs>
                       </Col>
                 </Row>
--- src/js/components/pc_usercenter.js
+++ src/js/components/pc_usercenter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Row, Col, Tabs, Upload, Icon, Modal } from 'antd';
+import { Row, Col, Tabs, Upload, Card, Icon, Modal } from 'antd';
 import PCHeader from "./pc_header";
 import PCFooter from "./pc_footer";
 const TabPane = Tabs.TabPane;
@@ -7,6 +7,8 @@ class PCUsercenter extends Component {
     constructor() {
         super();
         this.state = {
+            userCollection: '',
+            userComment: '',
             previewVisible: false,
             previewImage: '',
             fileList: [{
@@ -17,6 +19,28 @@ class PCUsercenter extends Component {
             }],
         }
 
+    }
+    componentDidMount() {
+        var myFetchOption = {
+            method: 'GET'
+        };
+        fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=getuc&userid=' + localStorage.userId, myFetchOption)
+            .then(response => response.json())
+            .then(json => {
+
+                this.setState({
+                    userCollection: json
+                });
+            })
+
+        fetch('http://newsapi.gugujiankong.com/Handler.ashx?action=getusercomments&userid=' + localStorage.userId, myFetchOption)
+            .then(response => response.json())
+            .then(json => {
+
+                this.setState({
+                    userComment: json
+                });
+            })
     }
     handleCancel = () => this.setState({
         previewVisible: false
@@ -41,6 +65,27 @@ class PCUsercenter extends Component {
         <div className="ant-upload-text">Upload</div>
       </div>
         );
+        const {userCollection, userComment} = this.state;
+        const collectionList = userCollection.length ?
+            userCollection.map((cl, index) => (
+                <Card style={{
+                    marginBottom: '5px'
+                }} key={index} title={cl.uniquekey} extra={<a target="_blank" href={`/details/${cl.uniquekey}`}>查看新闻详情</a>}>
+                <p>{cl.Title}</p>	
+                </Card>
+            ))
+            :
+            "暂时没有收藏内容！";
+        const commentList = userComment.length ?
+            userComment.map((ucl, index) => (
+                <Card style={{
+                    marginBottom: '5px'
+                }} key={index} title={`于 ${ucl.datetime} 评论了文章 ${ucl.uniquekey}`} extra={<a target="_blank" href={`/details/${ucl.uniquekey}`}>查看新闻详情</a>}>
+                <p>{ucl.Comments}</p>	
+                </Card>
+            ))
+            :
+            "暂时没有评论过任何新闻！";
         return (
             <div>
     			<PCHeader></PCHeader>
@@ -48,8 +93,8 @@ class PCUsercenter extends Component {
                     <Col span={2}></Col>
                     <Col span={20}>
                     	<Tabs defaultActiveKey="1" tabPosition="left">
-				          <TabPane tab="我的收藏列表" key="1">Content of tab 1</TabPane>
-				          <TabPane tab="我的评论列表" key="2">Content of tab 2</TabPane>
+				          <TabPane tab="我的收藏列表" key="1">{collectionList}</TabPane>
+				          <TabPane tab="我的评论列表" key="2">{commentList}</TabPane>
 				          <TabPane tab="头像设置" key="3">
 				          	<div className="clearfix">
 						        <Upload
